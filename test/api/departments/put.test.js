3524504ac57285b164c867e5e0e9d7da13f3e311
Agnieszka Falandys <agnfal@gmail.com>
2021-11-01 15:52:50 +0100

    add tests for
    endpoints - /api/departments

---
+++ test/api/departments/put.test.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../../server.js');
+const Department = require('../../../models/department.model');
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+const request = chai.request;
+
+describe('PUT /api/departments', () => {
+    before(async () => {
+        const testDepOne = new Department({ _id: '5d9f1140f10a81216cfd4408', name: 'Department #1' });
+        await testDepOne.save();
+      });
+
+    it('/:id should update chosen document and return success', async () => {
+       const res = await request(server).put('/api/departments/5d9f1140f10a81216cfd4408').send({ name: '#Department updated!' });
+       expect(res.status).to.be.equal(200);
+       expect(res.body.message).to.be.equal('The department was updated');
+
+       const updatedDep = await Department.findById('5d9f1140f10a81216cfd4408');
+       expect(updatedDep).to.not.be.null;
+       expect(updatedDep.name).to.be.equal('#Department updated!');
+    });
+  
+    after(async () => {
+        await Department.deleteMany();
+      });
+  });
